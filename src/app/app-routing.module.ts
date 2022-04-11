40c0d01778e21c310889f69245f6638fcdc24b0b
Oana Sabina Iancu <72797258+211IancuOanaSabina@users.noreply.github.com>
2022-04-12 00:59:28 +0300

    initialize readme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListBetsComponent } from './oders/list-bets/list-bets.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'matches', loadChildren: () => import('./matches/matches.module').then(m => m.MatchesModule) },
+  { path: 'bets', component:ListBetsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
